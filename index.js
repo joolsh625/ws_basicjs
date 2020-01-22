c07a6992e6478d99c89a532acdd3eeabc74b6886
jools <jools.hg@gmail.com>
2020-01-22 11:41:45 +0000

    Started excercises

--- index.js
+++ index.js
@@ -12,21 +12,76 @@
 'use strict';
 
 function id() {
-    return "Replace This With Your Student ID";
+    return "UP894547";
     // e.g. return "UP654321";
 }
 
 function fn() {
-    return "Replace This With Your First Name";
+    return "Jools";
 }
 
 function sn() {
-    return "Replace This With Your Surname";
+    return "Hattey";
 }
 
-function example() {
-    // replace this example with
-    // your first function then
-    // add more below as necessary.
-    return;
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function checkObject(object) {
+    object.checked = true;
+}
+
+function checkObjectInside(object) {
+    if(!(object.data === undefined)) {
+        checkObject(object.data);
+    }
+}
+
+function arraySet(array, number, x) {
+    if(!(array[number] == null)) {
+        array[number] = x;
+    }
+}
+
+function addAll(array) {
+    let reducer = (acc, cur) => acc + cur;
+    return array.reduce(reducer);
+}
+
+function larger(a, b) {
+    return Math.max(a, b);
+}
+
+function largest(array) {
+    let reducer = (acc, cur) => Math.max(acc, cur);
+    return array.reduce(reducer);
+}
+
+function compare(array1, array2) {
+    if(array1 === array2) {
+        return true;
+    }
+    if(array1.length =! array2.length) {
+        return false;
+    }
+    for(let i=0; i<array1.length; i++) {
+        if(array1[i]==!array2[i]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+function addToAll(array, x) {
+    if(array===undefine)
+    for(let i=0; i<array.length; i++) {
+        array[i] = array[i] + x;
+    }
+    return array;
 }
